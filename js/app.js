2cea4da221b1470488590a8a1e70df00834d9578
ibr <ibr.seckin@gmail.com>
2024-02-13 14:07:09 +0300

    homepage and testimonials buttons

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
+const testimonialsBtns = document.querySelectorAll(".testimonials-logos img");
+const testimonialsHeadImg = document.getElementById("testimonials-head-img");
 
+console.log(testimonialsHeadImg);
 
+testimonialsBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const logoToSend = btn.getAttribute("data");
+    console.log("logoToSend :>> ", logoToSend);
+    testimonialsHeadImg.setAttribute("src", `./img/logos/${logoToSend}.png`);
+
+    testimonialsBtns.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+    btn.classList.toggle("active");
+  });
+});
